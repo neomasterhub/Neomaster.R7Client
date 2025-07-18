fd966e7189e1e5d53712cbc8558e5373856e4cd1
Neomaster X <neomasterx@outlook.com>
2025-07-18 20:38:42 +0300

    Client registrar.

--- Neomaster.R7Client.Api/Program.cs
+++ Neomaster.R7Client.Api/Program.cs
@@ -1,13 +1,15 @@
 using System.Text.Json.Serialization;
-using Neomaster.R7Client.Api;
+using Neomaster.R7Client;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(
   options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Подключение клиента.
 builder.Services.AddR7Client(builder.Configuration);
-builder.Services.AddMemoryCache();
+builder.Services.AddMemoryCache(); // Буфер временного хранения исходных файлов для скачивания сервером документов.
 
 var app = builder.Build();
 app.UseSwagger();
---
+++ Neomaster.R7Client.Infra/R7ClientRegistrar.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Neomaster.R7Client.App;
+using Neomaster.R7Client.Infra;
+
+namespace Neomaster.R7Client;
+
+/// <summary>
+/// Регистратор зависимостей клиента.
+/// </summary>
+public static class R7ClientRegistrar
+{
+  /// <summary>
+  /// Регистрирует зависимости клиента для взаимодействия с API "Р7-Офис. Сервер документов".
+  /// <list type="bullet">
+  /// <item>
+  /// Регистрирует секцию конфигурации <see cref="R7ApiSettings"/>
+  /// как <see cref="IOptionsSnapshot{T}"/>.
+  /// </item>
+  /// <item>
+  /// Регистрирует <see cref="HttpClient"/> для обращения к API "Р7-Офис. Сервер документов"
+  /// с базовым адресом из <see cref="R7ApiSettings.ConvertUrl"/>.
+  /// </item>
+  /// </list>
+  /// </summary>
+  /// <param name="services">Сервисы приложения.</param>
+  /// <param name="configuration">Конфигурация приложения.</param>
+  /// <returns>Обогащенные сервисы приложения.</returns>
+  /// <exception cref="ArgumentNullException">Если в конфигурации отсутствует секция <see cref="R7ApiSettings"/>.</exception>
+  public static IServiceCollection AddR7Client(this IServiceCollection services, IConfiguration configuration)
+  {
+    var r7Settings = configuration.GetSection(nameof(R7ApiSettings)).Get<R7ApiSettings>();
+
+    if (r7Settings == null)
+    {
+      throw new ArgumentNullException(nameof(R7ApiSettings));
+    }
+
+    services.Configure<R7ApiSettings>(configuration.GetSection(nameof(R7ApiSettings)));
+    services.AddHttpClient<R7ApiClient>(client => { client.BaseAddress = new Uri(r7Settings.ConvertUrl); });
+    services.AddScoped<IR7ApiClient, R7ApiClient>();
+
+    return services;
+  }
+}
