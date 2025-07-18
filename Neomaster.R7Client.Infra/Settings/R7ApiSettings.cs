28831a440a45096ea232bb78b08903042c578441
Neomaster X <neomasterx@outlook.com>
2025-07-19 00:19:17 +0300

    Set R7ApiSettings as record.

--- Neomaster.R7Client.Infra/Settings/R7ApiSettings.cs
+++ Neomaster.R7Client.Infra/Settings/R7ApiSettings.cs
@@ -3,17 +3,17 @@ namespace Neomaster.R7Client.Infra;
 /// <summary>
 /// Настройки для работы с API "Р7-Офис. Сервер документов".
 /// </summary>
-public class R7ApiSettings
+public record R7ApiSettings
 {
   /// <summary>
   /// URL для конвертации документов.
   /// </summary>
-  public string ConvertUrl { get; set; } = string.Empty;
+  public string ConvertUrl { get; init; } = string.Empty;
 
   /// <summary>
   /// Используется в качестве подписи в JWT.
   /// Адрес значения: файл "/etc/r7-office/documentserver/local.json",
   /// параметр services.CoAuthoring.secret.inbox.string.
   /// </summary>
-  public string Secret { get; set; } = string.Empty;
+  public string Secret { get; init; } = string.Empty;
 }
