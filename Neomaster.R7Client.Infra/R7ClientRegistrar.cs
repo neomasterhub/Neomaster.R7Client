0dc05b0ebf0074a6ea29f965450bb586675649db
Neomaster X <neomasterx@outlook.com>
2025-07-19 00:19:46 +0300

    Lint AddR7Client().

--- Neomaster.R7Client.Infra/R7ClientRegistrar.cs
+++ Neomaster.R7Client.Infra/R7ClientRegistrar.cs
@@ -31,10 +31,9 @@ public static class R7ClientRegistrar
   {
     var r7Settings = configuration.GetSection(nameof(R7ApiSettings)).Get<R7ApiSettings>();
 
-    if (r7Settings == null)
-    {
-      throw new ArgumentNullException(nameof(R7ApiSettings));
-    }
+    ArgumentNullException.ThrowIfNull(r7Settings);
+    ArgumentNullException.ThrowIfNullOrEmpty(r7Settings.Secret);
+    ArgumentNullException.ThrowIfNullOrEmpty(r7Settings.ConvertUrl);
 
     services.Configure<R7ApiSettings>(configuration.GetSection(nameof(R7ApiSettings)));
     services.AddHttpClient<R7ApiClient>(client => { client.BaseAddress = new Uri(r7Settings.ConvertUrl); });
