02edae318a3efc14cf41370a48ea606ee0d7f154
Neomaster X <neomasterx@outlook.com>
2025-07-21 13:23:42 +0300

    Remove [DefaultValue]

--- Neomaster.R7Client.App/ApiClients/Models/R7DocPageMargins.cs
+++ Neomaster.R7Client.App/ApiClients/Models/R7DocPageMargins.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-
 namespace Neomaster.R7Client.App;
 
 /// <summary>
@@ -12,27 +10,23 @@ public record R7DocPageMargins
   /// Отступ слева.
   /// Значение по умолчанию: <see cref="R7ClientConsts.DocPageMargins.A4.Left"/>.
   /// </summary>
-  [DefaultValue(R7ClientConsts.DocPageMargins.A4.Left)]
   public string Left { get; init; } = R7ClientConsts.DocPageMargins.A4.Left;
 
   /// <summary>
   /// Отступ справа.
   /// Значение по умолчанию: <see cref="R7ClientConsts.DocPageMargins.A4.Right"/>.
   /// </summary>
-  [DefaultValue(R7ClientConsts.DocPageMargins.A4.Right)]
   public string Right { get; init; } = R7ClientConsts.DocPageMargins.A4.Right;
 
   /// <summary>
   /// Отступ сверху.
   /// Значение по умолчанию: <see cref="R7ClientConsts.DocPageMargins.A4.Top"/>.
   /// </summary>
-  [DefaultValue(R7ClientConsts.DocPageMargins.A4.Top)]
   public string Top { get; init; } = R7ClientConsts.DocPageMargins.A4.Top;
 
   /// <summary>
   /// Отступ снизу.
   /// Значение по умолчанию: <see cref="R7ClientConsts.DocPageMargins.A4.Bottom"/>.
   /// </summary>
-  [DefaultValue(R7ClientConsts.DocPageMargins.A4.Bottom)]
   public string Bottom { get; init; } = R7ClientConsts.DocPageMargins.A4.Bottom;
 }
--- Neomaster.R7Client.App/ApiClients/Models/R7DocPageSize.cs
+++ Neomaster.R7Client.App/ApiClients/Models/R7DocPageSize.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-
 namespace Neomaster.R7Client.App;
 
 /// <summary>
@@ -12,13 +10,11 @@ public record R7DocPageSize
   /// Ширина страницы.
   /// Значение по умолчанию: <see cref="R7ClientConsts.DocPageSize.A4.Width"/>.
   /// </summary>
-  [DefaultValue(R7ClientConsts.DocPageSize.A4.Width)]
   public string Width { get; init; } = R7ClientConsts.DocPageSize.A4.Width;
 
   /// <summary>
   /// Высота страницы.
   /// Значение по умолчанию:  <see cref="R7ClientConsts.DocPageSize.A4.Height"/>.
   /// </summary>
-  [DefaultValue(R7ClientConsts.DocPageSize.A4.Height)]
   public string Height { get; init; } = R7ClientConsts.DocPageSize.A4.Height;
 }
